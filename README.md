8faa06bc9dba6203191833083cc82a46300a9943
galoocego <55190799+galoocego@users.noreply.github.com>
2019-09-13 09:02:38 -0300

    Include hat

--- README.md
+++ README.md
@@ -2,30 +2,37 @@
 
 You can use on vRP or ESX
 <br>
-This script can be used for put and remove /mask
+This script can be used for put and remove /mask or /hat
 <br>
 Type on chat this command
 <br>
 Add esc_cmds on your resource.cfg start esc_cmds
 <br>
+<br>
 Usage example: 
 <br>
 Remove mask 
 <br>
+```lua
 /mask
+```
 <br>
 <br>
 
 Put Pig mask
 <br>
+```
 /mask 1
+```
 <br>
 <br>
 Put mask with color, 
 <br>
 where you will use mask 56 and color 7
 <br>
+```
 /mask 56 7
+```
 <br>
 <br>
-New commands will be add
+Use /hat [number] [color] for put hat, or /hat to remove
--- client.lua
+++ client.lua
@@ -17,3 +17,22 @@ RegisterCommand("mask", function(source, args)
         SetPedComponentVariation(PlayerPedId(), 1, 0, 0, 1)
     end
 end, false)
+
+-- Hat command
+RegisterCommand("hat", function(source, args)
+    -- If argument 1 provided
+    if args[1] then
+        -- Initialise color variable
+        local color = 0
+        -- If color number provided in argument 2
+        if args[2] then
+            -- Set color to provided argument
+            color = tonumber(args[2])
+        end
+
+        -- Update ped
+        SetPedPropIndex(GetPlayerPed(-1), 0, tonumber(args[1]), color, true);
+    else
+        SetPedPropIndex(GetPlayerPed(-1), 0, 0, 0, true);
+    end
+end, false)
